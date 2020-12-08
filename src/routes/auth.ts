d5857c2e4e604e5617d1cedb57ec780417a52f8b
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-08 12:44:13 -0800

    5) Logout route & auth middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken'
 import cookie from 'cookie'
 
 import { User } from '../entities/User'
+import auth from '../middleware/auth'
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body
@@ -74,29 +75,34 @@ const login = async (req: Request, res: Response) => {
     )
 
     return res.json(user)
-  } catch (error) {}
-}
-
-const me = async (req: Request, res: Response) => {
-  try {
-    const token = req.cookies.token
-    if (!token) throw new Error('Unauthenticated')
-
-    const { username }: any = jwt.verify(token, process.env.JWT_SECRET)
-
-    const user = await User.findOne({ username })
-
-    if (!user) throw new Error('Unauthenticated')
-
-    return res.json(user)
   } catch (error) {
     console.log(error)
-    return res.status(401).json({ error: error.message })
+    return res.status(500).json(error)
   }
+}
+
+const me = (_: Request, res: Response) => {
+  return res.json(res.locals.user)
+}
+
+const logout = (_: Request, res: Response) => {
+  res.set(
+    'Set-Cookie',
+    cookie.serialize('token', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      expires: new Date(0),
+      path: '/',
+    })
+  )
+
+  return res.status(200).json({ success: true })
 }
 
 const router = Router()
 router.post('/register', register)
 router.post('/login', login)
-router.get('/me', me)
+router.get('/me', auth, me)
+router.get('/logout', auth, logout)
 export default router
