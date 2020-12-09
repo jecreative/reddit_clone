8348637dffbc0d295ddfc277ed6dc8c7774d62f4
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-08 19:24:04 -0800

    17) Comment on post route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express'
 import auth from '../middleware/auth'
 import Post from '../entities/Post'
 import Sub from '../entities/Sub'
+import Comment from '../entities/Comment'
 
 const createPosts = async (req: Request, res: Response) => {
   const { title, body, sub } = req.body
@@ -55,8 +56,32 @@ const getPost = async (req: Request, res: Response) => {
   }
 }
 
+const commentOnPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params
+  const body = req.body.body
+
+  try {
+    const post = await Post.findOneOrFail({ identifier, slug })
+
+    const comment = new Comment({
+      body,
+      user: res.locals.user,
+      post,
+    })
+
+    await comment.save()
+
+    return res.json(comment)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({ error: 'Post not found' })
+  }
+}
+
 const router = Router()
 router.post('/', auth, createPosts)
 router.get('/', getPosts)
 router.get('/:identifier/:slug', getPost)
+router.post('/:identifier/:slug/comments', auth, commentOnPost)
+
 export default router
