3159563c41a329556db0e7c8226305be5cce5e10
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-10 12:26:00 -0800

    32) Register page functionality & validation

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,7 +1,43 @@
+import { FormEvent, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Axios from 'axios'
+import { useRouter } from 'next/router'
+
+import InputGroup from '../components/InputGroup'
 
 const Register = () => {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [agreement, setAgreement] = useState(false)
+  const [errors, setErrors] = useState<any>({})
+
+  const router = useRouter()
+
+  const submitForm = async (event: FormEvent) => {
+    event.preventDefault()
+
+    if (!agreement) {
+      setErrors({
+        ...errors,
+        agreement: 'You must agree to terms & conditions',
+      })
+      return
+    }
+
+    try {
+      await Axios.post('/auth/register', {
+        email,
+        password,
+        username,
+      })
+      router.push('/login')
+    } catch (error) {
+      setErrors(error.response.data)
+    }
+  }
+
   return (
     <div className='flex'>
       <Head>
@@ -18,38 +54,46 @@ const Register = () => {
           <p className='mb-10 text-xs'>
             By continuing, you agree to our User Agreement and Privacy Policy
           </p>
-          <form>
+          <form onSubmit={submitForm}>
             <div className='mb-6'>
               <input
                 type='checkbox'
                 className='mr-1 cursor-pointer'
                 id='agreement'
+                checked={agreement}
+                onChange={(e) => setAgreement(e.target.checked)}
               />
               <label htmlFor='agreement' className='text-xs cursor-pointer'>
                 I agree to get emails about cool stuff on Readit
               </label>
+              <small className='block font-medium text-red-600'>
+                {errors.agreement}
+              </small>
             </div>
-            <div className='mb-2'>
-              <input
-                type='email'
-                className='w-full p-3 transition duration-200 border border-gray-300 rounded outline-none focus:bg-white hover:bg-white bg-gray-50'
-                placeholder='Email'
-              />
-            </div>
-            <div className='mb-2'>
-              <input
-                type='text'
-                className='w-full p-3 transition duration-200 border border-gray-300 rounded outline-none focus:bg-white hover:bg-white bg-gray-50'
-                placeholder='Username'
-              />
-            </div>
-            <div className='mb-2'>
-              <input
-                type='password'
-                className='w-full p-3 transition duration-200 border border-gray-300 rounded outline-none focus:bg-white hover:bg-white bg-gray-50'
-                placeholder='Password'
-              />
-            </div>
+            <InputGroup
+              classname='mb-2'
+              type='email'
+              value={email}
+              setValue={setEmail}
+              placeholder='EMAIL'
+              error={errors.email}
+            />
+            <InputGroup
+              classname='mb-2'
+              type='text'
+              value={username}
+              setValue={setUsername}
+              placeholder='USERNAME'
+              error={errors.username}
+            />
+            <InputGroup
+              classname='mb-2'
+              type='password'
+              value={password}
+              setValue={setPassword}
+              placeholder='PASSWORD'
+              error={errors.password}
+            />
             <button className='w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border border-blue-500 rounded'>
               Sign Up
             </button>
