f86fa8f31091079d5eab0cdf9cde2f3d8768127b
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-12 12:04:55 -0800

    43) Navbar component & authRoute check

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,24 @@
-import '../styles/globals.css'
 import type { AppProps /*, AppContext */ } from 'next/app'
+import { useRouter } from 'next/router'
 import Axios from 'axios'
+
+import '../styles/tailwind.css'
+import Navbar from '../components/Navbar'
 
 Axios.defaults.baseURL = 'http://localhost:5000/api'
 Axios.defaults.withCredentials = true
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter()
+  const authRoutes = ['/register', '/login']
+  const authRoute = authRoutes.includes(pathname)
+
+  return (
+    <>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
